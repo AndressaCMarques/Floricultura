5f84a2076813d51257cc8bb3b7820d657bf91b10
andre <andre@ANDRESSA-PC>
2023-04-02 22:43:44 -0300

    Adicionar arquivos de projeto.

---
+++ Planta.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Floricultura
+{
+    internal class Planta
+    {
+        /*atributos: 
+        nome
+        preço
+        frequência rega
+        porte
+        sombra/sol
+        tóxica
+        */
+        string nome;
+        double preco;
+        int rega;
+        List<string> porte = new List<string>();
+        enum luminosidade
+        {
+            SolPleno,
+            MeiaSombra,
+            Sombra
+        }
+        bool toxicidade;
+        public Planta()
+        {
+            porte.Add("P");
+            porte.Add("M");
+            porte.Add("G");
+        } //string nome, double preco, int rega
+
+        //tipo retorno vai ser uma classe, nesse caso planta. Ela passa ser um objeto. Vamos ter todas as informações, seja ela string, bool, double... tudo dentro de Planta.
+        public Planta CadastroPlanta(Planta plantaAtualizar)
+        {
+            //Console.WriteLine("\nDigite o nome da planta:");
+            //plantaAtualizar.nome = Console.ReadLine();
+            //Console.WriteLine($"O nome da sua planta é {plantaAtualizar.nome}");
+
+            plantaAtualizar.nome = NomePlanta();
+
+            plantaAtualizar.preco = ValorPlanta(plantaAtualizar.nome);
+
+            plantaAtualizar.toxicidade = PlantaToxica(plantaAtualizar.nome, plantaAtualizar.preco);
+
+            //List<string> regaDoDia = PlantasParaRegar();
+
+            //Console.WriteLine("\nDigite o valor da planta:");
+            //plantaAtualizar.preco = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"O valor de {plantaAtualizar.nome} é {plantaAtualizar.preco}");
+
+            /*Console.WriteLine("\nA planta é tóxica? Responda \"sim\" ou \"não\"");
+            string plantaToxica = Console.ReadLine();
+            
+            if (plantaToxica == "sim")
+            {
+                plantaAtualizar.toxicidade = true;
+                Console.WriteLine("\nA planta selecionada é tóxica");
+            }
+            else if (plantaToxica == "não")
+            {
+                plantaAtualizar.toxicidade = false;
+                Console.WriteLine("\nA planta selecionada não é tóxica");
+            }
+            else
+            {
+                Console.WriteLine("\n Erro. Digite novamente:");
+            }
+
+            */
+            return plantaAtualizar;
+        }
+
+        public string NomePlanta()
+        {
+            Console.WriteLine("\nDigite o nome da planta:");
+            string nomeDaPlanta = Console.ReadLine();
+            Console.WriteLine($"O nome da sua planta é {nomeDaPlanta}");
+            return nomeDaPlanta;
+        }
+
+        public List<string> PlantasParaRegar() 
+        {
+            Console.WriteLine("\nvamos regas as plantas:");
+
+            string plantaRegada1 = NomePlanta();
+
+            string plantaRegada2 = NomePlanta();
+
+            string plantaRegada3 = NomePlanta();
+
+            List<string> plantasRegadas = new List<string>();
+            plantasRegadas.Add(plantaRegada1);
+            plantasRegadas.Add(plantaRegada2);
+            plantasRegadas.Add(plantaRegada3);
+            return plantasRegadas;
+        }
+
+        public double ValorPlanta(string nomePlanta)
+        {
+            Console.WriteLine("\nDigite o valor da planta:");
+            double valorDaPlanta = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"O valor de {nomePlanta} é {valorDaPlanta}");
+            return valorDaPlanta;
+        }
+
+        public bool PlantaToxica(string nomePlanta, double valorDaPlanta) 
+        {
+            Console.WriteLine("\nA planta é tóxica? Responda \"sim\" ou \"não\"");
+            string plantaToxica = Console.ReadLine();//o que o usuario respondeu
+            bool plantaToxicidade = false;
+
+            if (plantaToxica == "sim")
+            {
+                plantaToxicidade = true;
+                Console.WriteLine($"\nA planta {nomePlanta} tem o valor de {valorDaPlanta} e é tóxica");
+            }
+            else if (plantaToxica == "não")
+            {
+                plantaToxicidade = false;
+                Console.WriteLine($"\nA planta {nomePlanta} tem o valor de {valorDaPlanta} e não é tóxica");
+            }
+            else
+            {
+                Console.WriteLine("\n Erro. Digite novamente:");
+            }
+
+
+            return plantaToxicidade;
+        }
+
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Floricultura
+{
+    internal class Program // responsável por rodar o programa, contém Menu. Abrir nova classe para escrever atributo das plantas e não ficar cheio de informação dentro do código.
+                  /*int numero = 2;
+                    numero = numero + 5;
+                    Console.WriteLine(numero);o numero passa a ter o valor de 7.
+                  */
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Digite um inteiro para saber sua metade:");
+            int teste = Convert.ToInt32(Console.ReadLine());
+            double numeroEscolhido = CalculaMetade(teste);
+            Console.WriteLine($"a metade é: {numeroEscolhido}");
+            */
+            //double metade = CalculaMetade(5);
+
+            MenuPlantas();//stpe over: Mostrar Menu, ele vai para próxima linha. Step into: entra em MenuPlanta();.
+
+            bool mostraMenu = true; // verdadeiro seria opção 1 e 2 porque ainda quero mostrar o menu, mas se ela não quiser mais responder precisa mudar a condição para falso, e esa opção estará quando ela digitar 3, que é a opção sair do menu.
+            while (mostraMenu)
+            {
+                string resposta = Console.ReadLine(); // precisa ler a resposta do usuário e colocar em uma variável
+                switch (resposta)
+                {
+                    case "1":
+                        //Cadastrar a planta
+                        Planta plantaCadastradada = new Planta();
+
+                        plantaCadastradada = plantaCadastradada.CadastroPlanta(plantaCadastradada);
+
+                        
+
+
+                        mostraMenu = true;
+                        MenuPlantas();
+                        break;
+                    case "2":
+                        //Limpar console
+                        Console.Clear();
+                        mostraMenu = true;
+                        MenuPlantas();
+                        break;
+                    case "3":
+                        //Sair
+                        mostraMenu = false;
+                        break;//para voltar para o while. Para quebrar fora do switch
+                    default: //Quando as opções dadas não foram a resposta do usuário. No exemplo, se o usuário digitou 5.
+                        //mensagem de erro, dá opção de digitar novamente
+                        Console.Write("Opção inválida. Digite novamente:");
+                        mostraMenu = true;
+                        MenuPlantas();
+                        break;
+                }
+
+            }
+
+        }
+
+        private static void MenuPlantas() //porque não tenho nenhuma entrada. entrada vazia, parenteses vazio
+        {
+            Console.WriteLine($"\nDigite 1 para cadastrar uma planta:"); //ctrl + C copia a linha anterior sem precisar selecionar
+            Console.WriteLine($"Digite 2 para limpar console:");
+            Console.WriteLine($"Digite 3 para sair do programa:");
+
+        }
+
+
+        private static double CalculaMetade(int numero)
+        {
+            Console.WriteLine($"entrada: {numero}");
+            double metadeValor = 0;
+            metadeValor = numero / 2;
+            Console.WriteLine($"saida: {metadeValor}");
+
+            return metadeValor;
+        }
+
+
+    }
+}
